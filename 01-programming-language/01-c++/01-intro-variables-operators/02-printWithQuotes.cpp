bb26d7d93422648675e0e8f4ecd56c46fdc3afc3
Alok Shandilya <alok.shandilya10@gmail.com>
2023-04-28 11:06:55 +0530

    add printWithQuotes.cpp

---
+++ 01-programming-language/01-c++/01-intro-variables-operators/02-printWithQuotes.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(string x) {
+  cout << x << endl;
+  cout << "\"GeeksforGeeks\"" << endl;
+}
+
+int main() {
+  string x = "GeeksforGeeks";
+  print(x);
+  return 0;
+}
